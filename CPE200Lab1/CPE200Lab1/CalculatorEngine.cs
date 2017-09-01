ce3c1829d4c28f94ba237ec34ac96f34ccb37f87
settawat boriruklert <z.z.z.z.z.z.z.zenith@gmail.com>
2017-09-01 08:51:02 +0700

    oldwork

--- CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/CalculatorEngine.cs
@@ -106,7 +106,7 @@ namespace CPE200Lab1
                         result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
                         // split between integer part and fractional part
                         parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
+                        // if integer erpart length is already break max output, return error
                         if (parts[0].Length > maxOutputSize)
                         {
                             return "E";
